1013f7547af3626d70e57613697ad174089490a2
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-04-30 19:48:57 -0700

    Fixed error

--- src/main/java/com/discordgames/Game.java
+++ src/main/java/com/discordgames/Game.java
@@ -7,12 +7,13 @@ public class Game {
         // Room 1
         Puzzle r1p1 = new Puzzle("write", "Solve the enigma", "answer", "a thing happened");
         Puzzle r1p2 = new Puzzle("type", "what is the phone number?", "18002455356", "another thing happened");
-        Room r1 = new Room(r1p1);
+        Room r1 = new Room("Room 1");
+        r1.addPuzzle(r1p1);
         r1.addPuzzle(r1p2);
 
         // Room 2
         // more puzzles...
-        Room r2 = new Room();
+        Room r2 = new Room("Room2");
 
         // All Rooms
         manager = new RoomManager(r1);
