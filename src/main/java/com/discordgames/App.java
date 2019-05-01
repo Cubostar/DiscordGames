3cd878ef515df849e73d7a2a570ae0e7b0c8a343
Kabir Kwatra <kkwatra1@saddleback.edu>
2019-04-30 17:32:56 -0700

    Added code for bot to run with an event listener

--- src/main/java/com/discordgames/App.java
+++ src/main/java/com/discordgames/App.java
@@ -1,13 +1,29 @@
 package com.discordgames;
 
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws LoginException
     {
-        System.out.println( "Hello World!" );
+        // Token Stored as a Environment Variable
+        final String TOKEN = System.getenv("BoogalooToken");        
+        JDA bot = new JDABuilder(TOKEN)
+            .addEventListener(new RoomHandler())
+            .build(); // Builds builder into a JDA
+
+        // Wait for bot to ready
+        try {
+            bot.awaitReady();
+            System.out.println("JDA Bot " + bot.toString() + " has Loaded");
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/discordgames/RoomHandler.java
@@ -0,0 +1,12 @@
+package com.discordgames;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class RoomHandler extends ListenerAdapter {
+    
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+    }
+}
